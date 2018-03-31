538ff720fb9c2c894c9f4caecce6c35a35692d37
Balazs Mester <bm@trolexaporta.com>
2018-03-31 21:07:45 +0200

    Add basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeadersComponent } from './headers/headers.component';
+import { PostListComponent } from './blog/post-list/post-list.component';
+import { PostItemComponent } from './blog/post-item/post-item.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadersComponent,
+    PostListComponent,
+    PostItemComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
