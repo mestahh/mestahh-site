08b60febf5f4d936cda0ddee1e3fac94ca3bd16e
Balazs Mester <bm@trolexaporta.com>
2018-04-03 10:19:38 +0200

    Add some basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,15 @@ import { HeadersComponent } from './headers/headers.component';
 import { PostListComponent } from './blog/post-list/post-list.component';
 import { PostItemComponent } from './blog/post-item/post-item.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { BlogComponent } from './blog/blog.component';
+import {Router, RouterModule, Routes} from '@angular/router';
+import { MusicComponent } from './music/music.component';
 
+const appRoutes: Routes = [
+  { path: '', component: BlogComponent},
+  { path: 'blog', component: BlogComponent},
+  { path: 'music', component: MusicComponent}
+]
 
 @NgModule({
   declarations: [
@@ -15,10 +23,13 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     HeadersComponent,
     PostListComponent,
     PostItemComponent,
-    SidebarComponent
+    SidebarComponent,
+    BlogComponent,
+    MusicComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
